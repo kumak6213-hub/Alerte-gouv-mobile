7ee1bb942b917b287c2bcd76f774d1b99e9d8bdb
Humainlink <kumak6213@gmail.com>
2026-05-07 04:02:19 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+{
+  "expo": {
+    "name": "Alerte Gouv",
+    "slug": "alerte-gouv",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "android": {
+      "package": "com.housca.alertegouv",
+      "versionCode": 1
+    },
+    "extra": {
+      "eas": {
+        "projectId": "02f79585-f9da-4939-abcc-fd4365952e2a"
+      }
+    }
+  }
+}
